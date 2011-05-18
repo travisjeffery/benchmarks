49d95343ef8730ba62f243e1bba45901292f267c
Travis Jeffery <t@travisjeffery.com>
2011-05-18 00:20:54 -0400

    string concatenation

---
+++ ruby/string_concatenation.rb
@@ -0,0 +1,31 @@
+# string concatenation testing
+
+def f1 strs
+  result = ""
+  strs.each do |str|
+    result += str
+  end
+  return result
+end
+
+def f2 strs
+  result = ""
+  strs.each do |str|
+    result << str
+  end
+  return result
+end
+
+def f3 strs
+  return strs.join
+end
+
+{:f1 => :"+=", :f2 => :<<, :f3 => :join}.each do |t|
+  time = Time.now
+  method(t.first).call ["chunky", "bacon"] * 10_000
+  puts "%-6s %s" % [t.last, Time.now - time]
+end
+
+# join ftw, followed by <<, += took a pit stop along the way
+# would be interesting to see results for Permutations, i.e. not just ["chunky", "bacon"] over and over, more like: ["aaaaaa", "bbbbbb", "abbbbb", ...], and etc.
+
